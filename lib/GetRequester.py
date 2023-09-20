5bdd7c93047f67ab22669001003be01377b10eae
atenliao <atenliao@gmail.com>
2023-09-20 00:28:39 -0700

    complete GetRequester.py

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,13 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        contents_list=[]
+        contents = json.loads(self.get_response_body())
+        for content in contents:
+            contents_list.append(content)
+        
+        return contents_list
